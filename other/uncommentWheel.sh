dccfe98981189dc77886082c6fababd661e7a361
Regan McSorley <RMcSorley@whiteclarkegroup.com>
2020-11-20 09:06:06 +0000

    Updated uncommentWheel script

--- other/uncommentWheel.sh
+++ other/uncommentWheel.sh
@@ -2,7 +2,10 @@
 
 
 # Bash command to uncomment wheel group from sudoers
-tmpfile=$(echo $((1 + RANDOM % 10))) && \
-correctline=$(grep $'%wheel\tALL=(ALL)\tALL' /etc/sudoers | sed s/#\ //g ) && \
-(awk -v correctLine="$correctline" '{ if (NR == 107) print correctLine; else print $0}' /etc/sudoers ) > /tmp/$tmpfile && \
-(yes | cp -f /tmp/$tmpfile /etc/sudoers) && rm -rf /tmp/$tmpfile
+tmpfile=$(echo $((1 + RANDOM % 10))) 
+correctline=$(grep $'%wheel ALL=(ALL) ALL' /etc/sudoers | sed s/#\ //g )
+echo $correctline
+# Look at using grep -n to get the line number dynamically
+(awk -v correctLine="$correctline" '{ if (NR == 82) print correctLine; else print $0}' /etc/sudoers ) > /tmp/$tmpfile && \
+cat /tmp/$tmpfile
+(yes | cp -f /tmp/$tmpfile /etc/sudoers) && rm -rf /tmp/$tmpfile 

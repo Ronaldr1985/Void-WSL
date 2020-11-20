3d062fa95326b5079980d9ba58ca387658193f3b
Regan McSorley <RMcSorley@whiteclarkegroup.com>
2020-11-20 08:15:52 +0000

    Added bash script to uncomment wheel group from sudoers file

---
+++ other/uncommentWheel.sh
@@ -0,0 +1,5 @@
+# Bash command to uncomment wheel group from sudoers
+tmpfile=$(echo $((1 + RANDOM % 10))) && \
+correctline=$(grep $'%wheel\tALL=(ALL)\tALL' /etc/sudoers | sed s/#\ //g ) && \
+(awk -v correctLine="$correctline" '{ if (NR == 107) print correctLine; else print $0}' /etc/sudoers ) > /tmp/$tmpfile && \
+(yes | cp -f /tmp/$tmpfile /etc/sudoers) && rm -rf /tmp/$tmpfile

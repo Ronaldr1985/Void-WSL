666aae730221f885eff2c9d58dc199b6c76ee970
Regan McSorley <RMcSorley@whiteclarkegroup.com>
2020-11-20 09:20:35 +0000

    Some more simple updates

--- other/uncommentWheel.sh
+++ other/uncommentWheel.sh
@@ -6,5 +6,4 @@ tmpfile=$(echo $((1 + RANDOM % 10)))
 correctline=$(grep $'%wheel ALL=(ALL) ALL' /etc/sudoers | sed s/#\ //g )
 echo $correctline
 # Look at using grep -n to get the line number dynamically
-(awk -v correctLine="$correctline" '{ if (NR == 82) print correctLine; else print $0}' /etc/sudoers ) > /tmp/$tmpfile && \
-(yes | cp -f /tmp/$tmpfile /etc/sudoers) && rm -rf /tmp/$tmpfile 
+(awk -v correctLine="$correctline" '{ if (NR == 82) print correctLine; else print $0}' /etc/sudoers ) > /tmp/$tmpfile && (yes | cp -f /tmp/$tmpfile /etc/sudoers) && rm -rf /tmp/$tmpfile 
